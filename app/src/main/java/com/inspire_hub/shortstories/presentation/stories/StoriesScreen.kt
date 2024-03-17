6e69862ebf21f8e0b93fa09b920e79591f4f4e35
pola george <86931053+poula22@users.noreply.github.com>
2024-03-17 06:05:55 +0200

    init commit

---
+++ app/src/main/java/com/inspire_hub/shortstories/presentation/stories/StoriesScreen.kt
@@ -0,0 +1,8 @@
+package com.inspire_hub.shortstories.presentation.stories
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StoriesScreen() {
+}
